db1c295afdc8179d3dd82fa69ef2bc8991edfd91
Eugene <lehongthai2000@gmail.com>
2022-12-27 13:50:56 +0700

    add swipe review section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper";
+import { GoQuote } from "react-icons/go";
 
 const Home = () => {
    return (
@@ -13,8 +16,7 @@ const Home = () => {
                         <img className="img-fluid rounded-3" src="images/champagne-editted.png" alt="main banner" />
                         <div className="main-banner-content position-absolute">
                            <h5>CHAMPAGNE</h5>
-                           <p>From $999.00</p>
-                           <Link className="button">Buy Now</Link>
+                           {/* <Link className="button">Buy Now</Link> */}
                         </div>
                      </div>
                   </div>
@@ -25,9 +27,6 @@ const Home = () => {
                            <img className="img-fluid rounded-3" src="images/whisky-editted.png" alt="sub banner" />
                            <div className="sub-banner-content position-absolute">
                               <h5>Whisky</h5>
-                              <p>
-                                 From $599.00
-                              </p>
                            </div>
                         </div>
 
@@ -35,86 +34,13 @@ const Home = () => {
                            <img className="img-fluid rounded-3" src="images/whisky-editted.png" alt="sub banner" />
                            <div className="sub-banner-content position-absolute">
                               <h5>Vodka</h5>
-                              <p>
-                                 From $599.00 
-                              </p>
                            </div>
                         </div>
-
-                        {/* <div className="sub-banner position-relative">
-                           <img className="img-fluid rounded-3" src="images/whisky-editted.png" alt="sub banner" />
-                           <div className="sub-banner-content position-absolute">
-                              <h5>Cognac</h5>
-                              <p>
-                                 From $599.00 
-                              </p>
-                           </div>
-                        </div>
-
-                        <div className="sub-banner position-relative">
-                           <img className="img-fluid rounded-3" src="images/whisky-editted.png" alt="sub banner" />
-                           <div className="sub-banner-content position-absolute">
-                           <h5>Liquer</h5>
-                              <p>
-                                 From $599.00
-                              </p>
-                           </div>
-                        </div> */}
                      </div>
                   </div>
                </div>
             </div>
          </section>
-
-         {/* <section className="home-wrapper-2 py-5"> */}
-            {/* <div className="container-xxl"> */}
-               {/* <div className="row">
-                  <div className="col-12">
-                     <div className="service d-flex align-items-center justify-content-between">
-                        <div className="d-flex align-items-center gap-15">
-                           <img src="images/service.png" alt="services" />
-                           <div>
-                              <h6>Free Shipping</h6>
-                              <p className="mb-0">From all orders over $100</p>
-                           </div>
-                        </div>
-                        <div className="d-flex align-items-center gap-15">
-                           <img src="images/service-02.png" alt="services" />
-                           <div>
-                              <h6>Daily Offer</h6>
-                              <p className="mb-0">Save upto 25% off</p>
-                           </div>
-                        </div>
-                        <div className="d-flex align-items-center gap-15">
-                           <img src="images/service-03.png" alt="services" />
-                           <div>
-                              <h6>Support 24/7</h6>
-                              <p className="mb-0">Shop with an expert</p>
-                           </div>
-
-                           <div />
-                        </div>
-
-                        <div className="d-flex align-items-center gap-15">
-                           <img src="images/service-04.png" alt="services" />
-                           <div>
-                              <h6>Affordable Prices</h6>
-                              <p className="mb-0">Get Factory Default Price</p>
-                           </div>
-                        </div>
-                        <div className="d-flex align-items-center gap-15">
-                           <img src="images/service-05.png" alt="services" />
-                           <div>
-                              <h6>Secure Payments</h6>
-                              <p className="mb-0">100% Protected Payments</p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div> */}
-            {/* </div> */}
-         {/* </section> */}
-
          <section className="home-wrapper-3 py-5">
             <div className="container-xxl">
                <div className="row">
@@ -125,12 +51,60 @@ const Home = () => {
                </div>
             </div>
          </section>
+
+         <section className="home-wrapper-4 py-5">
+            <div className="container-xxl">
+               <div className="row">
+                  <div className="swiper-banner py-3 px-3">
+                     <Swiper
+                        style={{ "--swiper-navigation-color": "#fff", "--swiper-pagination-color": "#fff" }}
+                        speed={700}
+                        autoplay={{ delay: 3300, disableOnInteraction: false }}
+                        spaceBetween={15}
+                        loop={true}
+                        navigation={true}
+                        pagination={{ clickable: true }}
+                        modules={[Autoplay, Navigation, Pagination]}
+                        className="d-flex flex-column">
+                        <SwiperSlide className="text-white text-center pb-4 px-3">
+                           <span><GoQuote /></span>
+                           <h5 className="py-3 px-4 lh-base">This is the best wine shopping site I have ever seen in my whole life. This is the best wine shopping site I have ever seen in my whole life</h5>
+                           <h6>Thai Le (5-years customer)</h6>
+                        </SwiperSlide>
+                        <SwiperSlide className="text-white text-center pb-4 px-3">
+                           <span><GoQuote /></span>
+                           <h5 className="py-3 px-4">This is the best wine shopping site I have ever seen in my whole life</h5>
+                           <h6>Thai Le (5-years customer)</h6>
+                        </SwiperSlide>
+                        <SwiperSlide className="text-white text-center pb-4 px-3">
+                           <span><GoQuote /></span>
+                           <h5 className="py-3 px-4 lh-base">This is the best wine shopping site I have ever seen in my whole life. This is the best wine shopping site I have ever seen in my whole life</h5>
+                           <h6>Huy Nguyen (2-years customer)</h6>
+                        </SwiperSlide>
+                        <SwiperSlide className="text-white text-center pb-4 px-3">
+                           <span><GoQuote /></span>
+                           <h5 className="py-3 px-4">This is the best wine shopping site I have ever seen in my whole life</h5>
+                           <h6>Huy Nguyen (2-years customer)</h6>
+                        </SwiperSlide>
+                     </Swiper>
+                  </div>
+               </div>
+            </div>
+         </section>
+
+         <section className="home-wrapper-5 py-5">
+            <div className="container-xxl">
+               <div className="row">
+                  <div>
+                     <h1></h1>
+                  </div>
+               </div>
+            </div>
+         </section>
       </>
    );
 };
 
 export default Home;
 
-
-// https://www.img2go.com/
 // https://www.img2go.com/
